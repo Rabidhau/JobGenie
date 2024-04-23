6c456f484d6c13cf6aee76a2cf3b66ac4b9ac2f8
Prakash Shrestha <prakashrestha2075@gmail.com>
2024-04-23 09:00:31 +0545

    ✨ Feat: api integration listing

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const JobCard = () => {
+export const JobCard = ({ props }) => {
   return (
     <div className="mx-auto bg-white shadow-md rounded-lg overflow-hidden hover:rounded-xl hover:shadow-slate-300 transition-all">
       <div className="px-4 py-6">
@@ -10,20 +10,18 @@ export const JobCard = () => {
             src="https://via.placeholder.com/50"
             alt="Logo"
           />
-          <h2 className="text-lg font-semibold text-gray-800">
-            Graphic Designer / Video Editor Intern
-          </h2>
+          <div className="flex flex-col">
+            <h2 className="text-lg font-semibold text-gray-800">
+              {props.jobTitle}
+            </h2>
+            <span className="text-xs">{props.location}</span>
+          </div>
         </div>
         <div className="mt-4">
-          <p className="text-sm text-gray-600 mt-2">
-            PADSHALA INCORPORATION PVT LTD
-          </p>
-          <p className="text-sm text-gray-600 mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
-            quam pellentesque, ultrices lectus vel, euismod magna.
-          </p>
+          <p className="text-sm text-gray-600 mt-2">{props.company}</p>
+          <p className="text-sm text-gray-600 mt-2">{props.jobDetails}</p>
           <div className="flex justify-between items-center mt-4">
-            <p className="text-xs text-gray-400">2 hours ago</p>
+            <p className="text-sm text-gray-700">Last Date: {props.submitBy}</p>
             <button className="px-4 py-2 bg-blue-500 text-white text-xs font-semibold rounded hover:bg-blue-600 transition duration-300">
               Read More
             </button>
